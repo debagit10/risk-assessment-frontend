b12b82340e22db7a6b434752ef7f370ed86de70a
Josiah Elias <josiahelias11@gmail.com>
2024-09-27 19:36:10 +0100

    feat: user authentication api implemented

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -1,6 +1,11 @@
 import { Button, Paper, TextField, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import React, { useState } from "react";
+import axios from "axios";
+import API_URL from "../../Env";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 
 interface UserData {
   email: string;
@@ -8,6 +13,8 @@ interface UserData {
 }
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [userData, setUserData] = useState<UserData>({
     email: "",
     password: "",
@@ -18,12 +25,52 @@ const Login = () => {
     setUserData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const submit = () => {
-    console.log(userData);
+  const config = { headers: { "Content-type": "application/json" } };
+
+  const submit = async () => {
+    setLoading(true);
+    if (!userData.email || !userData.password) {
+      setLoading(false);
+      toast.warning("Please fill all fields", {
+        position: "top-center",
+        autoClose: 300,
+      });
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        `${API_URL.API_URL}/api/user/login`,
+        userData,
+        config
+      );
+
+      if (response.data.success) {
+        setLoading(false);
+        toast.success(response.data.success, {
+          position: "top-center",
+          autoClose: 1500,
+          onClose: () => navigate("/upload"),
+        });
+      } else {
+        setLoading(false);
+        toast.success(response.data.error, {
+          position: "top-center",
+          autoClose: 1500,
+        });
+      }
+    } catch (error) {
+      setLoading(false);
+      toast.error(error.response.data.error, {
+        position: "top-center",
+        autoClose: 1500,
+      });
+    }
   };
 
   return (
     <div className="flex justify-center  my-10 ">
+      <ToastContainer />
       <Paper elevation={4} sx={{ paddingX: "5%", paddingBottom: "5%" }}>
         <Typography variant="h6" fontWeight={700} sx={{ marginY: "5%" }}>
           Login
@@ -44,9 +91,24 @@ const Login = () => {
             onChange={handleChange}
           />
 
-          <Button variant="contained" onClick={submit}>
-            Login
+          <Button
+            variant="contained"
+            onClick={submit}
+            disabled={loading}
+            sx={{ textTransform: "capitalize" }}
+          >
+            {loading ? "Logging in..." : "Login"}
           </Button>
+
+          <Typography variant="body2">
+            Don't have an account?{" "}
+            <span
+              onClick={() => navigate("/signup")}
+              className="underline cursor-pointer"
+            >
+              Sign up
+            </span>
+          </Typography>
         </Stack>
       </Paper>
     </div>
--- frontend/src/pages/auth/Signup.tsx
+++ frontend/src/pages/auth/Signup.tsx
@@ -1,5 +1,10 @@
 import { Paper, Stack, TextField, Button, Typography } from "@mui/material";
 import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import API_URL from "../../Env";
 
 interface UserData {
   email: string;
@@ -7,6 +12,8 @@ interface UserData {
 }
 
 const Signup = () => {
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [userData, setUserData] = useState<UserData>({
     email: "",
     password: "",
@@ -17,12 +24,53 @@ const Signup = () => {
     setUserData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const submit = () => {
-    console.log(userData);
+  const config = { headers: { "Content-type": "application/json" } };
+
+  const submit = async () => {
+    setLoading(true);
+    if (!userData.email || !userData.password) {
+      setLoading(false);
+      toast.warning("Please fill all fields", {
+        position: "top-center",
+        autoClose: 300,
+      });
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        `${API_URL.API_URL}/api/user/register`,
+        userData,
+        config
+      );
+
+      if (response.data.success) {
+        setLoading(false);
+        toast.success(response.data.success, {
+          position: "top-center",
+          autoClose: 1500,
+          onClose: () => navigate("/upload"),
+        });
+      } else {
+        setLoading(false);
+        toast.success(response.data.error, {
+          position: "top-center",
+          autoClose: 1500,
+        });
+      }
+    } catch (error) {
+      setLoading(false);
+
+      toast.error(error.response.data.error, {
+        position: "top-center",
+        autoClose: 1500,
+      });
+    }
   };
 
   return (
     <div className="flex justify-center  my-10 ">
+      <ToastContainer />
       <Paper elevation={4} sx={{ paddingX: "5%", paddingBottom: "5%" }}>
         <Typography variant="h6" fontWeight={700} sx={{ marginY: "5%" }}>
           Sign up
@@ -43,9 +91,24 @@ const Signup = () => {
             onChange={handleChange}
           />
 
-          <Button variant="contained" onClick={submit}>
-            Sign up
+          <Button
+            variant="contained"
+            onClick={submit}
+            disabled={loading}
+            sx={{ textTransform: "capitalize" }}
+          >
+            {loading ? "Signing up..." : "Sign up"}
           </Button>
+
+          <Typography variant="body2">
+            Already have an account?{" "}
+            <span
+              onClick={() => navigate("/")}
+              className="underline cursor-pointer"
+            >
+              Login
+            </span>
+          </Typography>
         </Stack>
       </Paper>
     </div>
