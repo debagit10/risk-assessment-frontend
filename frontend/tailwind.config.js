cda6e15635293db85e3c5b853de81fbabce7eb3f
Josiah Elias <josiahelias11@gmail.com>
2024-09-28 13:24:23 +0100

    feat: vercel.json file added

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "custom-bg": "url('/src/images/bg_image.jpg')",
+      },
+    },
   },
   plugins: [],
 };
